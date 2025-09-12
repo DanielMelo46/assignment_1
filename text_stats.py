7b75f0693bdc77e784d0dd1580f4c87aa03935ee
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-09-12 14:16:16 -0500

    update: Adding the word_count function

--- text_stats.py
+++ text_stats.py
@@ -1,22 +1,27 @@
-
 def read_file(file_path):
     '''Reads the content of a file and returns it as a string.'''
     with open(file_path, 'r') as file:
         return file.read()
     
 def remove_char(text):
+    '''Removes specific characters from the text.'''
     chars_to_remove = [',','.','\n']
     for char in chars_to_remove:
         text = text.replace(char,"")
     return text
 
-#def word_count(text):
+def word_count(text):
+    '''Counts the number of words in the text.
+    It uses the remove_char function to clean the text first.'''
+    words_list = remove_char(text).split(" ")
+    return len(words_list)
+
     
 # Read data from the file called input.txt
 # Read it into a variable
 file_content = read_file('inputs/input_1.txt')
 print(file_content)
-print(remove_char(file_content))
+print(word_count(file_content))
 
 
 
