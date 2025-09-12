700bc107c5412024ac51bc089139756b7cfb8f78
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-09-12 13:47:21 -0500

    update: Adding a remove_char function

--- text_stats.py
+++ text_stats.py
@@ -1,11 +1,22 @@
+
 def read_file(file_path):
     '''Reads the content of a file and returns it as a string.'''
     with open(file_path, 'r') as file:
         return file.read()
+    
+def remove_char(text):
+    chars_to_remove = [',','.','\n']
+    for char in chars_to_remove:
+        text = text.replace(char,"")
+    return text
 
+#def word_count(text):
+    
 # Read data from the file called input.txt
+# Read it into a variable
 file_content = read_file('inputs/input_1.txt')
 print(file_content)
-# Read it into a variable
+print(remove_char(file_content))
+
 
 
